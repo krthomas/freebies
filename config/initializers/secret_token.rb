9a601382b6d6994da4692b2325e9558cc01585e9
Kathryn Thomas <kathryn.thomas@sv.cmu.edu>
2011-09-23 20:57:31 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Freebies::Application.config.secret_token = '97c4d208f12f1ffc62050f1e22808c6eee368b847cce7f624d7a2f7d64348e5c0f299cb2ef1b79edf68afb709d2497770e7c669bc1597f30feceeacbc1ed1277'
